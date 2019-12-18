dc476e764220df0a3ad650be7e89e9194d7d0512
Indunil1992 <indunil@adroitlogic.com>
2019-12-18 10:53:50 +0530

    1

---
+++ gcpinddu/SqlConnMgr.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+    this.dbConnections = [];
+
+    this.dbConnections["indunilid"] = {
+        host: "35.190.147.46",
+        port: "3306",
+        user: "mysql.sys",
+        password: process.env.Password_sqlindunilid,
+        database: "information_schema"
+    };
+};
